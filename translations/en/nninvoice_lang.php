fb9ab3212feb0c06b80606d4f93c1ba062ed23dd
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-29 19:08:09 +0530

    Delete translations/en directory

--- translations/en/nninvoice_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$sLangName = "English";
-
-$aLang = [
-    'charset'                           => 'UTF-8',
-   'check'                              => 'this is credit card checking purpose',
-];
